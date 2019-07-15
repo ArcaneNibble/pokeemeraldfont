5c56a580b1c1810a4ce5e7e1d3469c3c8690dad0
Robert Ou <rqou@robertou.com>
2019-07-15 03:21:36 -0700

    Delete old unusued build script

--- build.sh
+++
@@ -1,6 +0,0 @@
-wget 'https://github.com/google/fonts/raw/master/apache/roboto/Roboto-Regular.ttf'
-ttx -f Roboto-Regular.ttf
-mv Roboto-Regular.ttx PokeIconEmerald.ttx
-
-
-ttx -f PokeIconEmerald.ttx
